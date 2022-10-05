9f12e5944b86a825098b6765cd0bed6ae7c5b0a1
BoyPika <52831958+BoyPika@users.noreply.github.com>
2022-10-04 21:20:23 -0400

    Update BringBackLuckForgeModItems.java

--- src/main/java/net/boypika/bringbackluckforge/init/BringBackLuckForgeModItems.java
+++ src/main/java/net/boypika/bringbackluckforge/init/BringBackLuckForgeModItems.java
@@ -1,1 +1,22 @@
+package net.boypika.bringbackluckforge.init;
 
+import net.boypika.bringbackluckforge.item.LuckyCloverItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class BringBackLuckForgeModItems {
+    public static final DeferredRegister<Item> REGISTRY;
+    public static final RegistryObject<Item> LUCKY_CLOVER;
+
+    public BringBackLuckForgeModItems() {
+    }
+
+    static {
+        REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, "bring_back_luck_forge");
+        LUCKY_CLOVER = REGISTRY.register("lucky_clover", () -> {
+            return new LuckyCloverItem();
+        });
+    }
+}
